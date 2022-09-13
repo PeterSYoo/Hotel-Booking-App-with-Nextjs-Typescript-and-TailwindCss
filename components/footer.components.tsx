b784988f1fc87a28178ab11156bd186c51bae57b
Peter Yoo <trustintofu@gmail.com>
2022-09-13 12:46:22 -0700

    wip: adds footer and styling

--- components/footer.components.tsx
+++ components/footer.components.tsx
@@ -1,5 +1,35 @@
+import Image from 'next/image';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex justify-between px-48 pt-44 pb-32 bg-gray-100">
+      <div className="w-64">
+        <div className="">
+          <Image src="/img/logo.png" alt="" width="148" height="38" />
+        </div>
+        <div className="pt-7">
+          <span className="text-gray-400">
+            This is the team that specializes in making sure in the find it a
+            your interior looks cool
+          </span>
+        </div>
+        <div className="w-32 h-10 mt-7 pl-3 pt-1 pr-2 flex items-center gap-x-7 bg-white rounded-lg">
+          <Image src="/img/button-sun.png" alt="" width="60" height="30" />
+          <Image src="/img/button-moon.png" alt="" width="24" height="24" />
+        </div>
+      </div>
+      <div className="">
+        <ul className="flex flex-col gap-3.5">
+          <li className="text-2xl">Business</li>
+          <li className="text-gray-400">Success</li>
+          <li className="text-gray-400">About Locato</li>
+          <li className="text-gray-400">Blog</li>
+          <li className="text-gray-400">Information</li>
+          <li className="text-gray-400">Travel Guide</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- components/header.components.tsx
+++ components/header.components.tsx
@@ -3,19 +3,23 @@ import Image from 'next/image';
 const Header = () => {
   return (
     <div className="flex py-5 px-10 justify-between items-center">
+      {/* Desktop */}
       <div className="hidden md:flex">
         <Image src="/img/logo.png" alt="" width="148" height="38" />
       </div>
-      <div className="md:hidden flex">
+      {/* Mobile */}
+      <div className="flex md:hidden">
         <Image src="/img/logo.png" alt="" width="96" height="28" />
       </div>
-      <div className="hidden md:flex gap-x-12 items-center">
+      {/* Desktop */}
+      <div className="hidden gap-x-12 items-center md:flex">
         <div className="text-xl">Login</div>
         <div className="text-xl text-white bg-blueBtn px-5 py-3 rounded-xl">
           signup
         </div>
       </div>
-      <div className="md:hidden flex gap-x-12 items-center">
+      {/* Mobile */}
+      <div className="flex gap-x-12 items-center md:hidden">
         <div className="text-base">Login</div>
         <div className="text-base text-white bg-blueBtn px-4 py-2 rounded-xl">
           signup
