1b2c21482fe2b968b407297cf31176f0c33cfd87
Peter Yoo <trustintofu@gmail.com>
2022-09-15 19:51:41 -0700

    wip: adds trending cities section for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -752,6 +752,205 @@ const Home: NextPage = () => {
           </div>
         </div>
       </section>
+
+      {/* Trending Cities */}
+      <section className="bg-[#F5F5F5]">
+        <section className="max-w-375 mx-auto md:pl-[135px] md:mx-0 md:max-w-1170">
+          <div className="mt-[60px] mb-[12px] md:mb-[24px] md:mt-[100px]">
+            <h1 className="text-center text-[30px] font-bold leading-10 md:text-left md:text-[48px]">
+              Trending Cities
+            </h1>
+          </div>
+          <div className="mb-[35px]">
+            <p className="max-w-375 w-600 text-center text-[14px] leading-6 text-gray-400 md:max-w-1440 md:text-left md:px-0">
+              The most searched for cities on TripGuide
+            </p>
+          </div>
+        </section>
+
+        <section className="max-w-375 mx-auto md:hidden">
+          {/* Card */}
+          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto">
+            <div className="flex mx-[16px] my-[16px] gap-[16px]">
+              <div className="w-[160px] h-[168px] bg-gray-300 rounded-2xl"></div>
+              <div className="w-[127px] h-[168px] flex flex-col justify-between -mt-1">
+                <h1 className="text-[24px] font-bold">Manila</h1>
+                <div className="flex items-center">
+                  <Image
+                    src="/img/explore-the-world/star.png"
+                    alt=""
+                    width="14"
+                    height="14"
+                  />
+                  <span className="pl-[8px]">4.91</span>
+                  <span className="text-gray-400">&nbsp;(147)</span>
+                </div>
+                <div className="">
+                  <span className="text-[18px]">
+                    $250.00
+                    <span className="text-gray-400">/night</span>
+                  </span>
+                </div>
+                <div className="">
+                  <span className="bg-blue-600 text-white py-[4.5px] px-[12px] rounded-md">
+                    Book now
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Card */}
+          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto">
+            <div className="flex mx-[16px] my-[16px] gap-[16px]">
+              <div className="w-[160px] h-[168px] bg-gray-300 rounded-2xl"></div>
+              <div className="w-[127px] h-[168px] flex flex-col justify-between -mt-1">
+                <h1 className="text-[24px] font-bold">Manila</h1>
+                <div className="flex items-center">
+                  <Image
+                    src="/img/explore-the-world/star.png"
+                    alt=""
+                    width="14"
+                    height="14"
+                  />
+                  <span className="pl-[8px]">4.91</span>
+                  <span className="text-gray-400">&nbsp;(147)</span>
+                </div>
+                <div className="">
+                  <span className="text-[18px]">
+                    $250.00
+                    <span className="text-gray-400">/night</span>
+                  </span>
+                </div>
+                <div className="">
+                  <span className="bg-blue-600 text-white py-[4.5px] px-[12px] rounded-md">
+                    Book now
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Card */}
+          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto">
+            <div className="flex mx-[16px] my-[16px] gap-[16px]">
+              <div className="w-[160px] h-[168px] bg-gray-300 rounded-2xl"></div>
+              <div className="w-[127px] h-[168px] flex flex-col justify-between -mt-1">
+                <h1 className="text-[24px] font-bold">Manila</h1>
+                <div className="flex items-center">
+                  <Image
+                    src="/img/explore-the-world/star.png"
+                    alt=""
+                    width="14"
+                    height="14"
+                  />
+                  <span className="pl-[8px]">4.91</span>
+                  <span className="text-gray-400">&nbsp;(147)</span>
+                </div>
+                <div className="">
+                  <span className="text-[18px]">
+                    $250.00
+                    <span className="text-gray-400">/night</span>
+                  </span>
+                </div>
+                <div className="">
+                  <span className="bg-blue-600 text-white py-[4.5px] px-[12px] rounded-md">
+                    Book now
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Card */}
+          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto">
+            <div className="flex mx-[16px] my-[16px] gap-[16px]">
+              <div className="w-[160px] h-[168px] bg-gray-300 rounded-2xl"></div>
+              <div className="w-[127px] h-[168px] flex flex-col justify-between -mt-1">
+                <h1 className="text-[24px] font-bold">Manila</h1>
+                <div className="flex items-center">
+                  <Image
+                    src="/img/explore-the-world/star.png"
+                    alt=""
+                    width="14"
+                    height="14"
+                  />
+                  <span className="pl-[8px]">4.91</span>
+                  <span className="text-gray-400">&nbsp;(147)</span>
+                </div>
+                <div className="">
+                  <span className="text-[18px]">
+                    $250.00
+                    <span className="text-gray-400">/night</span>
+                  </span>
+                </div>
+                <div className="">
+                  <span className="bg-blue-600 text-white py-[4.5px] px-[12px] rounded-md">
+                    Book now
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Card */}
+          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto">
+            <div className="flex mx-[16px] my-[16px] gap-[16px]">
+              <div className="w-[160px] h-[168px] bg-gray-300 rounded-2xl"></div>
+              <div className="w-[127px] h-[168px] flex flex-col justify-between -mt-1">
+                <h1 className="text-[24px] font-bold">Manila</h1>
+                <div className="flex items-center">
+                  <Image
+                    src="/img/explore-the-world/star.png"
+                    alt=""
+                    width="14"
+                    height="14"
+                  />
+                  <span className="pl-[8px]">4.91</span>
+                  <span className="text-gray-400">&nbsp;(147)</span>
+                </div>
+                <div className="">
+                  <span className="text-[18px]">
+                    $250.00
+                    <span className="text-gray-400">/night</span>
+                  </span>
+                </div>
+                <div className="">
+                  <span className="bg-blue-600 text-white py-[4.5px] px-[12px] rounded-md">
+                    Book now
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Card */}
+          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto">
+            <div className="flex mx-[16px] my-[16px] gap-[16px]">
+              <div className="w-[160px] h-[168px] bg-gray-300 rounded-2xl"></div>
+              <div className="w-[127px] h-[168px] flex flex-col justify-between -mt-1">
+                <h1 className="text-[24px] font-bold">Manila</h1>
+                <div className="flex items-center">
+                  <Image
+                    src="/img/explore-the-world/star.png"
+                    alt=""
+                    width="14"
+                    height="14"
+                  />
+                  <span className="pl-[8px]">4.91</span>
+                  <span className="text-gray-400">&nbsp;(147)</span>
+                </div>
+                <div className="">
+                  <span className="text-[18px]">
+                    $250.00
+                    <span className="text-gray-400">/night</span>
+                  </span>
+                </div>
+                <div className="">
+                  <span className="bg-blue-600 text-white py-[4.5px] px-[12px] rounded-md">
+                    Book now
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
     </div>
   );
 };
