a4ef8811abb7e5d84d130aedd6073181e12b71ed
Peter Yoo <trustintofu@gmail.com>
2022-09-15 20:08:47 -0700

    wip: adds trending cities section for medium screens and up

--- pages/index.tsx
+++ pages/index.tsx
@@ -755,22 +755,20 @@ const Home: NextPage = () => {
 
       {/* Trending Cities */}
       <section className="bg-[#F5F5F5]">
-        <section className="max-w-375 mx-auto md:pl-[135px] md:mx-0 md:max-w-1170">
-          <div className="mt-[60px] mb-[12px] md:mb-[24px] md:mt-[100px]">
-            <h1 className="text-center text-[30px] font-bold leading-10 md:text-left md:text-[48px]">
-              Trending Cities
-            </h1>
-          </div>
+        <section className="max-w-375 mx-auto md:max-w-1170">
+          <h1 className="text-center text-3xl px-9 pt-7 pb-3 font-bold leading-10 md:text-5xl md:max-w-1170 md:px-0 md:mx-auto md:mt-[20px]">
+            Trending Cities
+          </h1>
           <div className="mb-[35px]">
-            <p className="max-w-375 w-600 text-center text-[14px] leading-6 text-gray-400 md:max-w-1440 md:text-left md:px-0">
+            <p className="max-w-375 mx-auto w-600 text-center text-[14px] leading-6 text-gray-400 md:max-w-1440 md:text-[24px] md:leading-3 md:mt-[16px] md:mb-[50px]">
               The most searched for cities on TripGuide
             </p>
           </div>
         </section>
 
-        <section className="max-w-375 mx-auto md:hidden">
+        <section className="max-w-375 mx-auto md:flex md:gap-[40px] md:max-w-[980px] md:flex-wrap">
           {/* Card */}
-          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto">
+          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto md:w-[470px]">
             <div className="flex mx-[16px] my-[16px] gap-[16px]">
               <div className="w-[160px] h-[168px] bg-gray-300 rounded-2xl"></div>
               <div className="w-[127px] h-[168px] flex flex-col justify-between -mt-1">
@@ -800,7 +798,7 @@ const Home: NextPage = () => {
             </div>
           </div>
           {/* Card */}
-          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto">
+          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto md:w-[470px]">
             <div className="flex mx-[16px] my-[16px] gap-[16px]">
               <div className="w-[160px] h-[168px] bg-gray-300 rounded-2xl"></div>
               <div className="w-[127px] h-[168px] flex flex-col justify-between -mt-1">
@@ -830,7 +828,7 @@ const Home: NextPage = () => {
             </div>
           </div>
           {/* Card */}
-          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto">
+          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto md:w-[470px]">
             <div className="flex mx-[16px] my-[16px] gap-[16px]">
               <div className="w-[160px] h-[168px] bg-gray-300 rounded-2xl"></div>
               <div className="w-[127px] h-[168px] flex flex-col justify-between -mt-1">
@@ -860,7 +858,7 @@ const Home: NextPage = () => {
             </div>
           </div>
           {/* Card */}
-          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto">
+          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto md:w-[470px]">
             <div className="flex mx-[16px] my-[16px] gap-[16px]">
               <div className="w-[160px] h-[168px] bg-gray-300 rounded-2xl"></div>
               <div className="w-[127px] h-[168px] flex flex-col justify-between -mt-1">
@@ -890,7 +888,7 @@ const Home: NextPage = () => {
             </div>
           </div>
           {/* Card */}
-          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto">
+          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto md:w-[470px]">
             <div className="flex mx-[16px] my-[16px] gap-[16px]">
               <div className="w-[160px] h-[168px] bg-gray-300 rounded-2xl"></div>
               <div className="w-[127px] h-[168px] flex flex-col justify-between -mt-1">
@@ -920,7 +918,7 @@ const Home: NextPage = () => {
             </div>
           </div>
           {/* Card */}
-          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto">
+          <div className="flex w-335 h-[201px] mb-[20px] bg-white rounded-3xl mx-auto md:w-[470px]">
             <div className="flex mx-[16px] my-[16px] gap-[16px]">
               <div className="w-[160px] h-[168px] bg-gray-300 rounded-2xl"></div>
               <div className="w-[127px] h-[168px] flex flex-col justify-between -mt-1">
