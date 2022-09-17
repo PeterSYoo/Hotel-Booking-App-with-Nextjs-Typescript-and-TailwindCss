584f9601e2289e93c3e6c71d90ec5405cf9e0ceb
Peter Yoo <trustintofu@gmail.com>
2022-09-16 18:45:16 -0700

    wip: adds styling to medium and up for login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,11 +1,9 @@
 const Login = () => {
   return (
     <div className="bg-gray-400 h-screen font-dmSans pt-10">
-      {/* Mobile */}
-
-      <section className="max-w-375 mx-auto">
-        <div className="w-[324px] bg-white rounded-2xl px-[30px] py-[30px]">
-          <div className="text-right -mt-10 -mr-10">
+      <section className="max-w-375 mx-auto md:max-w-1140">
+        <div className="w-[324px] bg-white rounded-2xl px-[30px] py-[30px] md:w-[448px] md:px-[40px]">
+          <div className="text-right -mt-10 -mr-10 md:-mr-12">
             <button
               type="button"
               className="bg-white rounded-3xl p-1 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 drop-shadow"
@@ -29,44 +27,46 @@ const Login = () => {
             </button>
           </div>
           <h1 className="text-center text-[30px] font-bold">Welcome Back!</h1>
-          <div className="flex justify-between mt-[20px]">
-            <div className="w-[221px] h-[34px] bg-blue-500 text-center text-[12px] py-2 text-white rounded-md">
+          <div className="flex justify-between mt-[20px] gap-[12px]">
+            <div className="bg-blue-600 text-center text-[12px] text-white py-[8px] rounded-md w-full md:text-[20px]">
               Sign in with Google
             </div>
-            <div className="w-[34px] h-[34px] bg-gray-600 rounded-md"></div>
+            <div className="w-[34px] h-[34px] bg-gray-600 rounded-md md:w-[48px] md:h-[48px]"></div>
           </div>
           <div className="flex mx-auto justify-center items-center">
             <div className="border-b w-[69px] h-[1px] mt-[22px]"></div>
-            <p className="text-[10px] mt-[22px] mx-3 text-gray-400">
+            <p className="text-[10px] mt-[22px] mx-3 text-gray-400 md:text-[14px]">
               or continue with
             </p>
             <div className="border-b w-[69px] h-[1px] mt-[22px]"></div>
           </div>
           <div className="mt-[12px]">
             <form>
-              <div className="text-[10px] text-gray-600">Email address</div>
+              <div className="text-[10px] text-gray-600 md:text-[14px]">
+                Email address
+              </div>
               <input
                 type="text"
                 placeholder="Enter your email"
-                className="bg-gray-200 text-[10px] rounded-md py-[10px] pl-[10px] w-full mt-[6px]"
+                className="bg-gray-200 text-[10px] rounded-md py-[10px] pl-[10px] w-full mt-[6px] md:text-[14px] md:py-[13px]"
               />
-              <div className="text-[10px] text-gray-600 mt-[14px]">
+              <div className="text-[10px] text-gray-600 mt-[14px] md:text-[14px]">
                 Password
               </div>
               <input
                 type="text"
                 placeholder="Enter your password"
-                className="bg-gray-200 text-[10px] rounded-md py-[10px] pl-[10px] w-full mt-[6px]"
+                className="bg-gray-200 text-[10px] rounded-md py-[10px] pl-[10px] w-full mt-[6px] md:text-[14px] md:py-[13px]"
               />
-              <div className="text-[10px] text-gray-600 mt-[12px] text-right">
+              <div className="text-[10px] text-gray-600 mt-[12px] text-right md:text-[14px]">
                 Forgot your password?
               </div>
               <input
                 type="submit"
                 value="Sign in"
-                className="bg-blue-600 text-center text-white py-[8px] rounded-md mt-[15px] w-full"
+                className="bg-blue-600 text-center text-white py-[8px] rounded-md mt-[15px] w-full md:text-[20px]"
               />
-              <div className="text-center text-[10px] mt-[20px]">
+              <div className="text-center text-[10px] mt-[20px] text-gray-600 md:text-[14px]">
                 Don&apos;t have an account? Sign up
               </div>
             </form>
