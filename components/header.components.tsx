71cf64fa0d1e42e9fd151669f587f972af7aba86
Peter Yoo <trustintofu@gmail.com>
2022-09-19 21:27:33 -0700

    wip: adds modal to show login page when clicking on login

--- components/header.components.tsx
+++ components/header.components.tsx
@@ -1,52 +1,59 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { getSession, useSession, signOut } from 'next-auth/react';
+import { useSession, signOut } from 'next-auth/react';
 import { FaRegBell } from 'react-icons/fa';
+import Login from '../pages/login';
+import { useState } from 'react';
 
 const handleSignOut = () => {
   signOut();
 };
 
 const Guest = () => {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
-    <div className="flex py-[25px] px-10 justify-between items-center max-w-1440 mx-auto bg-white font-dmSans">
-      {/* Desktop */}
-      <div className="hidden items-center md:flex">
-        <Image src="/img/logo.png" alt="" width="40" height="38" />
-        <span className="text-xl font-openSans font-bold pl-2.5">
-          TripGuide
-        </span>
-      </div>
-      {/* Mobile */}
-      <div className="flex items-center md:hidden">
-        <Image src="/img/logo.png" alt="" width="28" height="28" />
-        <span className="text-xs font-openSans font-bold pl-2.5">
-          TripGuide
-        </span>
-      </div>
-      {/* Desktop */}
-      <div className="hidden gap-x-12 items-center md:flex">
-        <div className="text-xl font-bold">
-          <Link href={'/login'}>
-            <a>Login</a>
-          </Link>
+    <>
+      <Login open={openModal} onClose={() => setOpenModal(false)} />
+      <div className="flex py-[25px] px-10 justify-between items-center max-w-1440 mx-auto bg-white font-dmSans">
+        {/* Desktop */}
+        <div className="hidden items-center md:flex">
+          <Image src="/img/logo.png" alt="" width="40" height="38" />
+          <span className="text-xl font-openSans font-bold pl-2.5">
+            TripGuide
+          </span>
         </div>
-        <button className="text-xl text-white bg-blueBtn px-5 py-3 rounded-xl">
-          <Link href={'/signup'}>
-            <a>signup</a>
-          </Link>
-        </button>
+        {/* Mobile */}
+        <div className="flex items-center md:hidden">
+          <Image src="/img/logo.png" alt="" width="28" height="28" />
+          <span className="text-xs font-openSans font-bold pl-2.5">
+            TripGuide
+          </span>
+        </div>
+        {/* Desktop */}
+        <div className="hidden gap-x-12 items-center md:flex">
+          <div className="text-xl font-bold">
+            <span onClick={() => setOpenModal(true)} className="cursor-pointer">
+              Login
+            </span>
+          </div>
+          <button className="text-xl text-white bg-blueBtn px-5 py-3 rounded-xl">
+            <Link href={'/signup'}>
+              <a>signup</a>
+            </Link>
+          </button>
+        </div>
+        {/* Mobile */}
+        <div className="flex gap-x-4 items-center md:hidden">
+          <div className="text-base">Login</div>
+          <button className="text-base text-white bg-blueBtn px-2 py-1 rounded-xl">
+            <Link href={'/signup'}>
+              <a>signup</a>
+            </Link>
+          </button>
+        </div>
       </div>
-      {/* Mobile */}
-      <div className="flex gap-x-4 items-center md:hidden">
-        <div className="text-base">Login</div>
-        <button className="text-base text-white bg-blueBtn px-2 py-1 rounded-xl">
-          <Link href={'/signup'}>
-            <a>signup</a>
-          </Link>
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
