8659c318a6010bcb2f50172d36b0a8f49545e486
Andre Gonzales <gibstock@gmail.com>
2022-10-20 12:54:02 -0700

    added tanstack/react-query, setup query providers, made successful test fetch endpoint, installed json-server at port 4000

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,22 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <div className="bg-sectionBg">
-        <Head>
-          <title>TripGuide</title>
-        </Head>
-        <Component {...pageProps} />
-      </div>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={pageProps.session}>
+        <div className="bg-sectionBg">
+          <Head>
+            <title>TripGuide</title>
+          </Head>
+          <Component {...pageProps} />
+        </div>
+      </SessionProvider>
+    </QueryClientProvider>
   );
 }
 
