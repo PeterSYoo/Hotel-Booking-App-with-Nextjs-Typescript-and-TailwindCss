4600bd773af3bcee0289569e23d7c6e79585fade
Peter Yoo <trustintofu@gmail.com>
2022-10-21 09:44:46 -0700

    refactor: Removes google fonts from layout component.

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,16 +9,6 @@ export const Layout = ({ children }: any) => {
       <Head>
         <title>TripGuide</title>
         <meta name="description" content="Hotel booking app" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&family=Open+Sans:wght@400;700&family=Poppins:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <div className="min-h-screen min-w-screen flex flex-col">
         <Header />
