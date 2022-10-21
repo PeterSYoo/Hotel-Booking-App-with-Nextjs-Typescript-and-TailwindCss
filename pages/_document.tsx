459361b640925bf5787ffba66b9917f60945af63
Peter Yoo <trustintofu@gmail.com>
2022-10-21 09:44:57 -0700

    fix: Adds google fonts to document.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,18 @@ const Document = () => {
   return (
     <>
       <Html>
-        <Head></Head>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&family=Open+Sans:wght@400;700&family=Poppins:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
