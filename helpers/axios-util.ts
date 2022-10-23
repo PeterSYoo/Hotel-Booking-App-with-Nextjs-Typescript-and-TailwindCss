6535d747b3db245290a0182f45118be1fb16e9e2
Peter Yoo <102557183+PeterSYoo@users.noreply.github.com>
2022-10-22 18:49:57 -0700

    Revert "switch from using json server to nextjs api route to fetch travel data"

--- helpers/axios-util.ts
+++ helpers/axios-util.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: 'http://localhost:3000/api/db'
+  baseURL: 'http://localhost:4000'
 })
 
 export const request = ({...options}) => {
