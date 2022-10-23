7e723f84157280c69719246077a4560631af67d1
Andre Gonzales <gibstock@gmail.com>
2022-10-22 17:41:42 -0700

    switch from using json server to nextjs api route to fetch travel data

--- helpers/axios-util.ts
+++ helpers/axios-util.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: 'http://localhost:4000'
+  baseURL: 'http://localhost:3000/api/db'
 })
 
 export const request = ({...options}) => {
--- pages/fetch-test/index.tsx
+++ pages/fetch-test/index.tsx
@@ -2,21 +2,25 @@ import type { NextPage } from "next"
 import { useQuery } from "@tanstack/react-query"
 import { request } from "../../helpers/axios-util"
 import Image from 'next/image'
+import axios from "axios"
 
 type PlaceProps = {
   country: string,
   img: string
 }
 
-const fetchTravelData = () => {
-  return request({url: '/best-place'}) // Change per endpoint
-}
+// const fetchTravelData = () => {
+//   return request({url: '/best-place'}) // Change per endpoint
+// }
 
+const fetchTravelData = () => {
+  return axios.get('http://localhost:3000/api/db/db')
+}
 const FetchTest: NextPage = () => {
 
   const {data: places, isInitialLoading} = useQuery(['travel-data'], fetchTravelData) // use alias for data destructure
   
-  console.log("data",places)
+  console.log("data", places?.data["best-place"])
 
   if(isInitialLoading) return <h2>Loading...</h2>
 
@@ -24,7 +28,7 @@ const FetchTest: NextPage = () => {
     <div className="w-full flex flex-col justify-center items-center gap-6">
       <h1>Data Fetch Test</h1>
       {
-        places?.data.map((place: PlaceProps) => (
+        places?.data["best-place"].map((place: PlaceProps) => (
           <div key={place.country} className='relative'>
             <h2 className="absolute left-1/2 bottom-3 -translate-x-1/2 text-white font-bold text-4xl z-[2]">{place.country}</h2>
             <div className="overlay absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-black z-[1]"></div>
