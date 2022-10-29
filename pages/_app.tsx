5fe41b2c45be3f7c31a1df903fa3fbc18a635def
Peter Yoo <trustintofu@gmail.com>
2022-10-28 22:29:07 -0700

    feat: Adds Hydrate wrapper around our entire app from react query.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,25 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { Layout } from "../components/layout";
-import { SessionProvider } from "next-auth/react";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Layout } from '../components/layout';
+import { SessionProvider } from 'next-auth/react';
+import {
+  QueryClientProvider,
+  QueryClient,
+  Hydrate,
+} from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <SessionProvider session={pageProps.session}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </SessionProvider>
+      <Hydrate state={pageProps.dehydratedState}>
+        <SessionProvider session={pageProps.session}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SessionProvider>
+      </Hydrate>
     </QueryClientProvider>
   );
 }
