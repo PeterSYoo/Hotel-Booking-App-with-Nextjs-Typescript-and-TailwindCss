f354718d185d8c4b04438b48b82d8495de84e1c5
Peter Yoo <trustintofu@gmail.com>
2022-10-28 22:32:24 -0700

    feat: Implements useReducer to be used to create an object with our required key value pairs, and passed into our useMutation to post the object data to our database.

--- pages/test-route/index.tsx
+++ pages/test-route/index.tsx
@@ -1,15 +1,42 @@
-import { useQuery } from '@tanstack/react-query';
+import {
+  dehydrate,
+  QueryClient,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
+import { useReducer, useState } from 'react';
+import { AddTest } from '../../components/test-route/addTest.components';
+
 import { deleteTest, getTests } from '../../lib/testsHelper';
 
-const Testing = () => {
-  const { isLoading, isError, data, error } = useQuery(['tests'], getTests);
+const formReducer = (state: any, event: any) => {
+  return {
+    ...state,
+    [event.target.name]: event.target.value,
+  };
+};
 
-  if (isLoading) return <div className="mx-10 mt-10">Tests are loading..</div>;
+const TestRoute = () => {
+  const queryClient = useQueryClient();
+  const [formData, setFormData] = useReducer(formReducer, {});
+  // Fetches the data stored in the cache so no loading state.
+  const { isError, data, error } = useQuery(['tests'], getTests);
+
   if (isError) return <div className="mx-10 mt-10">Got an error..</div>;
+
+  const handleDelete = (testId: any) => {
+    deleteTest(testId);
+    queryClient.refetchQueries(['tests']);
+  };
 
   return (
     <>
       <div className="mx-20 mt-10">
+        {/* Add form */}
+        <div className="flex flex-col mb-10 px-6 py-4 bg-gray-700 rounded-lg w-3/4 mx-auto border-b-2 border-gray-600">
+          <AddTest formData={formData} setFormData={setFormData} />
+        </div>
         {data.map((test: any) => (
           <div key={test._id}>
             <div className="flex flex-col pb-3 mb-3 border-b border-gray-600">
@@ -31,7 +58,7 @@ const Testing = () => {
                 <div className="col-start-7 col-span-6">
                   <div className="flex justify-end gap-10">
                     <button>Edit</button>
-                    <button onClick={(e: any) => deleteTest(test._id)}>
+                    <button onClick={(e: any) => handleDelete(test._id)}>
                       Delete
                     </button>
                   </div>
@@ -45,4 +72,17 @@ const Testing = () => {
   );
 };
 
-export default Testing;
+export default TestRoute;
+
+export const getServerSideProps = async () => {
+  const queryClient = new QueryClient();
+
+  // We prefetch the data and store it in the cache
+  await queryClient.prefetchQuery(['tests'], getTests);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
