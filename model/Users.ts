3e1ce22717049d0e9fae7285b998bc6ae2e07e01
Peter Yoo <trustintofu@gmail.com>
2022-11-02 16:07:59 -0700

    feat: Adds more fields to our mongoose user schema.

--- model/Users.ts
+++ model/Users.ts
@@ -1,12 +1,16 @@
-import { Schema, model, models } from "mongoose";
+import { Schema, model, models } from 'mongoose';
 
 const userSchema = new Schema({
   firstName: String,
   lastName: String,
   email: String,
   password: String,
+  cityState: String,
+  streetAddress: String,
+  dateOfBirth: Date,
+  gender: String,
 });
 
-const Users = models.user || model("user", userSchema);
+const Users = models.user || model('user', userSchema);
 
 export default Users;
