68250d0011ee3207871a2e20b69c109b1879f1bf
Peter Yoo <trustintofu@gmail.com>
2022-11-01 19:29:50 -0700

    feat: Adds placeholder.com as image domain to be used in image components.

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['res.cloudinary.com']
-  }
+    domains: ['res.cloudinary.com', 'via.placeholder.com'],
+  },
 };
 
 module.exports = nextConfig;
