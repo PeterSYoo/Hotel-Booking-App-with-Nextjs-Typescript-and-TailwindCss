14b89b2db0ef483e9a1f8d5da31b828806614e7a
Peter Yoo <trustintofu@gmail.com>
2022-11-07 19:17:20 -0800

    feat: Edits theme button component to work with next themes.

--- components/theme-button.components.tsx
+++ components/theme-button.components.tsx
@@ -1,31 +1,45 @@
-import { useEffect } from 'react';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 import { BsFillSunFill, BsMoonStars } from 'react-icons/bs';
-import { applyThemePreference } from '../applyThemePreference';
-import useThemeStore from '../stores/useThemeStore';
+// import { applyThemePreference } from '../applyThemePreference';
+// import useThemeStore from '../stores/useThemeStore';
 
 export default function ThemeButton() {
-  const toggleTheme = useThemeStore((state: any) => state.toggleTheme);
-  const theme = useThemeStore((state: any) => state.theme);
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    applyThemePreference(theme);
-  }, [theme]);
+    setMounted(true);
+  }, []);
+  // const toggleTheme = useThemeStore((state: any) => state.toggleTheme);
+  // const theme = useThemeStore((state: any) => state.theme);
 
-  if (theme === 'dark') {
-    return (
-      <>
-        <button onClick={toggleTheme} type="button">
-          <BsFillSunFill size={25} />
-        </button>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <button onClick={toggleTheme} type="button">
-          <BsMoonStars size={25} />
-        </button>
-      </>
-    );
-  }
+  // useEffect(() => {
+  //   applyThemePreference(theme);
+  // }, [theme]);
+  const renderThemeChanger = () => {
+    if (!mounted) return null;
+
+    const currentTheme = theme === 'system' ? systemTheme : theme;
+
+    if (currentTheme === 'dark') {
+      return (
+        <>
+          <button onClick={() => setTheme('light')} type="button">
+            <BsFillSunFill size={25} />
+          </button>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <button onClick={() => setTheme('dark')} type="button">
+            <BsMoonStars size={25} />
+          </button>
+        </>
+      );
+    }
+  };
+
+  return <>{renderThemeChanger()}</>;
 }
